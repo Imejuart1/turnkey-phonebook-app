043ca6fafcdf20c32aefd7de6d22be637570d776
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2025-02-08 15:18:01 +0100

    created various components like navabr and contact page

---
+++ src/app/components/toggle-view/toggle-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-view',
+  imports: [],
+  templateUrl: './toggle-view.component.html',
+  styleUrl: './toggle-view.component.scss'
+})
+export class ToggleViewComponent {
+
+}
